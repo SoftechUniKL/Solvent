3b2e737aa54d4ed13d504dfb32ac948a446bc0cd
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2015-05-12 15:27:24 +0200

    Verbessern des Datumsformats.

---
+++ Solvent/src/BudgetPlan.java
@@ -0,0 +1,12 @@
+/**
+ * Anwendung BudgetPlan
+ * 
+ */
+
+public class BudgetPlan {
+	public static void main(String[] args) {
+		BudgetPlanModel budget = new BudgetPlanModel(); // Modell
+		new BudgetPlanGUI(budget); // View und Controller
+	}
+
+}
---
+++ Solvent/src/BudgetPlanModel.java
@@ -0,0 +1,51 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import com.opencsv.CSVReader;
+
+/**
+ * Datenmodell des Budgetplaners
+ * 
+ * Die Daten werden in der Datei data/budget.csv abgespeichert als CSV-Datei.
+ * 
+ */
+public class BudgetPlanModel {
+	List<Posten> ausgaben;
+
+	public BudgetPlanModel() {
+		this.ausgaben = new ArrayList<Posten>();
+		try {
+			// Zeilenweises Einlesen der Daten
+			CSVReader reader = new CSVReader(new FileReader("data/budget.csv"));
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+				DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.GERMAN);
+				Date datum = df.parse(nextLine[0]);
+				String bezeichnung = nextLine[1];
+				double betrag = Double.parseDouble(nextLine[2]);
+				ausgaben.add(new Posten(datum, bezeichnung, betrag));
+			}
+			reader.close();
+
+		} catch (FileNotFoundException e) {
+			System.err
+					.println("Die Datei data/budget.csv wurde nicht gefunden!");
+			System.exit(1);
+		} catch (IOException e) {
+			System.err
+					.println("Probleme beim Oeffnen der Datei data/budget.csv!");
+			System.exit(1);
+		} catch (ParseException e) {
+			System.err
+					.println("Formatfehler: Die Datei konnte nicht eingelesen werden!");
+			System.exit(1);
+		}
+	}
+}
