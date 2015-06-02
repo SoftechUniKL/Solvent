207f3e281e07bb1c331605069a0a55f72abcab28
jmetzger <jonas@horcher-metzger.de>
2015-06-02 16:08:01 +0200

    nochmal

--- Solvent/src/csvWriter.java
+++ Solvent/src/csvWriter.java
@@ -3,25 +3,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class csvWriter {
-	int i = 0;
-	public static void main(String[] Args){
+	
+
+	public static void main(String[] Args) {
+		
 		csvWriter test = new csvWriter();
 		test.writer();
 		System.out.println("Ich funktioniere!");
+		
 	}
-	public void writer() { //Hier Übergabe der Eingaben... Posten aus Eingabe erstellen, oder erst beim Einlesen?
+
+	public void writer() { // Hier Übergabe der Eingaben... Posten aus Eingabe
+							// erstellen, oder erst beim Einlesen?
 		
 		FileWriter fw;
 		try {
-			fw = new FileWriter("data/budget.csv",true);
-		
-			
+			fw = new FileWriter("data/budget.csv", true);
+
 			BufferedWriter bw = new BufferedWriter(fw);
-			String test = "13.12.2015,Testausgabe"+i+",299.90\n";
+			String test = "13.12.2015,Testausgabe,299.90\n";
 			bw.write(test);
-			i++;
+
 			bw.close();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
