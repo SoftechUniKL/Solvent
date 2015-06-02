bbb262cf8e7c021d039e182eb314c665193e8c63
jmetzger <jonas@horcher-metzger.de>
2015-06-02 15:56:37 +0200

    hi

--- Solvent/src/BudgetPlan.java
+++ Solvent/src/BudgetPlan.java
@@ -5,7 +5,9 @@
 
 public class BudgetPlan {
 	public static void main(String[] args) {
+	
 		BudgetPlanModel budget = new BudgetPlanModel(); // Modell
+		
 		new BudgetPlanGUI(budget); // View und Controller
 	}
 
--- Solvent/src/csvWriter.java
+++ Solvent/src/csvWriter.java
@@ -3,23 +3,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class csvWriter {
-	
+	int i = 0;
 	public static void main(String[] Args){
 		csvWriter test = new csvWriter();
 		test.writer();
 		System.out.println("Ich funktioniere!");
 	}
-	public void writer() {
-
+	public void writer() { //Hier Übergabe der Eingaben... Posten aus Eingabe erstellen, oder erst beim Einlesen?
+		
 		FileWriter fw;
 		try {
-			fw = new FileWriter("data/testbudget.csv");
-
+			fw = new FileWriter("data/budget.csv",true);
+		
+			
 			BufferedWriter bw = new BufferedWriter(fw);
-			String test = "13.12.2015,Testausgabe, 299.90";
+			String test = "13.12.2015,Testausgabe"+i+",299.90\n";
 			bw.write(test);
+			i++;
 			bw.close();
-
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
