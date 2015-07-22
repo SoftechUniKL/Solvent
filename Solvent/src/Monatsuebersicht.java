0bbd0899419cf381718a14592a617d0693bc7d90
JuliaSchmit <jul.schmitt@gmx.de>
2015-07-22 18:29:09 +0200

    Optische Anpassung

--- Solvent/src/Monatsuebersicht.java
+++ Solvent/src/Monatsuebersicht.java
@@ -1,11 +1,19 @@
+import java.awt.Button;
+import java.awt.Canvas;
 import java.awt.EventQueue;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JRadioButton;
@@ -29,12 +37,92 @@ import java.util.Date;
 import java.util.Calendar; 
 
 public class Monatsuebersicht extends JFrame {
-/*	public static void main(String[]args)	{
-		// Ausgabe des Array
-	    for (String[] arr : einnahmen) {
-	        System.out.println(Arrays.toString(arr));
+	
+	private JPanel contentPane;
+	private JTextField txtJahresbersicht;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
+	
+	public Monatsuebersicht()	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 594, 383);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		Button button = new Button("Start");
+		menuBar.add(button);
+		
+		Button button_1 = new Button("Ausgaben");
+		menuBar.add(button_1);
+		
+		Button button_2 = new Button("Einnahmen");
+		menuBar.add(button_2);
+		
+		Button button_3 = new Button("Monats\u00FCbersicht");
+		menuBar.add(button_3);
+		
+		Button button_4 = new Button("Jahres\u00FCbersicht");
+		menuBar.add(button_4);
+		
+		Button button_5 = new Button("Planung");
+		menuBar.add(button_5);
+		
+		Button button_6 = new Button("Sparen");
+		menuBar.add(button_6);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtJahresbersicht = new JTextField();
+		txtJahresbersicht.setHorizontalAlignment(SwingConstants.CENTER);
+		txtJahresbersicht.setText("Jahres\u00FCbersicht");
+		txtJahresbersicht.setBounds(140, 11, 303, 20);
+		contentPane.add(txtJahresbersicht);
+		txtJahresbersicht.setColumns(10);
+		
+		JTextPane txtpnJahresgesamteinnahmen = new JTextPane();
+		txtpnJahresgesamteinnahmen.setText("Jahresgesamteinnahmen");
+		txtpnJahresgesamteinnahmen.setBounds(243, 58, 148, 20);
+		contentPane.add(txtpnJahresgesamteinnahmen);
+		
+		JTextPane txtpnJahresgesamtausgaben = new JTextPane();
+		txtpnJahresgesamtausgaben.setText("Jahresgesamtausgaben");
+		txtpnJahresgesamtausgaben.setBounds(243, 91, 148, 20);
+		contentPane.add(txtpnJahresgesamtausgaben);
+		
+		textField = new JTextField();
+		textField.setBounds(401, 58, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(401, 91, 86, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setText("\u20AC");
+		textPane.setBounds(497, 58, 17, 20);
+		contentPane.add(textPane);
+		
+		JTextPane textPane_1 = new JTextPane();
+		textPane_1.setText("\u20AC");
+		textPane_1.setBounds(497, 91, 17, 20);
+		contentPane.add(textPane_1);
+		
+		table = new JTable();
+		table.setBounds(30, 58, 177, 222);
+		contentPane.add(table);
+		
+		Canvas canvas = new Canvas();
+		canvas.setBounds(236, 117, 278, 156);
+		contentPane.add(canvas);
 	}
-*/	
+	
+	
 	public String readCSVEinnahmen(int r, int c)	{
 		String [][] einnahmen = new String [12][3];
 		String [][] datum = new String[13][3];
